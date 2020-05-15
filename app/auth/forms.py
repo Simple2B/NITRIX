af758fc86885e930de3e7106219deca3b2eb878b
RootenberG <savichevdenis244@gmail.com>
2020-05-15 14:34:01 +0300

    Added base and login templates, fixed tests

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,6 +6,6 @@ from ..models import User
 
 
 class LoginForm(FlaskForm):
-    user_id = StringField('Username or Email', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
-    submit = SubmitField('Login')
+    user_id = StringField("Name", [DataRequired()])
+    password = PasswordField("Password", [DataRequired()])
+    submit = SubmitField("Login")
