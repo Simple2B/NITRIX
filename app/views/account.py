69815023ec3bcab49eb0ffeb8992ec48cbff89b2
Nikolay Chernov <nchernov@gmail.com>
2020-05-18 14:21:57 +0300

    fill account edit

--- app/views/account.py
+++ app/views/account.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from flask import render_template, Blueprint, request, flash, redirect, url_for
 
-from app.models import Account
+from app.models import Account, Product, Reseller
 
 
 account_blueprint = Blueprint('account', __name__)
@@ -9,11 +10,24 @@ account_blueprint = Blueprint('account', __name__)
 @account_blueprint.route("/account_details")
 def edit():
     if 'id' in request.args:
-        id = request.args['id']
+        id = int(request.args['id'])
         account = Account.query.filter(Account.id == id).first()
         if account is None:
             flash("Wrong account id.", "danger")
             return redirect(url_for('main.accounts'))
-        return render_template("account_details.html")
+        return render_template(
+                "account_details.html",
+                product=Product,
+                reseller=Reseller,
+                account=account,
+                is_edit=True
+            )
+        # return render_template(
+        #         "account_details.html",
+        #         product=Product,
+        #         reseller=Reseller,
+        #         today=datetime.now(),
+        #         is_edit=False
+        #     )
     flash("Need account id.", "danger")
     return redirect(url_for('main.accounts'))
