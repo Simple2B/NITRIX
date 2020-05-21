bfb66bf07e1a0dea667cb668275fd52b58cf7db1
Nikolay Chernov <nchernov@gmail.com>
2020-05-21 20:32:18 +0300

    history account changes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .reseller import Reseller  # noqa
 from .account import Account  # noqa
 from .product import Product  # noqa
 from .account_extensions import AccountExtension  # noqa
+from .account_changes import AccountChanges  # noqa
