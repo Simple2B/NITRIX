b167653c888f05c3e213741e6842e8afc00e58aa
MeitaArt <meitaartem@gmail.com>
2020-05-25 10:52:16 +0300

    routes

--- app/views/auth.py
+++ app/views/auth.py
@@ -3,14 +3,13 @@ from flask_login import login_user, logout_user, login_required
 
 from ..models import User
 from ..forms import LoginForm
-from app.logger import log
+from ..forms import ChangePasswordForm
 
 auth_blueprint = Blueprint("auth", __name__)
 
 
 @auth_blueprint.route("/login", methods=["GET", "POST"])
 def login():
-    log(log.INFO, '/login')
     form = LoginForm(request.form)
     if form.validate_on_submit():
         user = User.authenticate(form.user_name.data, form.password.data)
@@ -19,15 +18,23 @@ def login():
             flash("Login successful.", "success")
             return redirect(url_for("main.index"))
         flash("Wrong user name or password.", "danger")
-        log(log.WARNING, "Invalid user data.")
     return render_template("login.html", form=form)
 
 
 @auth_blueprint.route("/logout")
 @login_required
 def logout():
-    log(log.DEBUG, '/logout')
     logout_user()
     flash("You were logged out.", "info")
-    log(log.INFO, 'User logged out.')
     return redirect(url_for("auth.login"))
+
+
+@auth_blueprint.route("/change_password")
+def change_password():
+    form = ChangePasswordForm()
+    return render_template("change_password.html", form=form)
+
+
+def check_password(password):
+    return not(len(password) < 8 or password.isdigit() or password.isdigit()
+               or password.isalpha() or password.islower() or password.isupper())
