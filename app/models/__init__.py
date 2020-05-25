ad58be318a27063b980fb4c97d50f566ef7d74fe
Nikolay <nchernov@gmail.com>
2020-05-25 20:04:39 +0300

    model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from .user import User  # noqa
 from .reseller import Reseller  # noqa
 from .account import Account  # noqa
 from .product import Product  # noqa
+from .phone import Phone  # noqa
 from .account_extensions import AccountExtension  # noqa
 from .account_changes import AccountChanges  # noqa
 from .reseller_product import ResellerProduct  # noqa
