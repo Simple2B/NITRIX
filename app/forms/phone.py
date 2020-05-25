d4cc8f12ebf47687c3a33b2f4ec098886f4adb66
Nikolay <nchernov@gmail.com>
2020-05-25 20:05:16 +0300

    form

---
+++ app/forms/phone.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+
+
+class PhoneForm(FlaskForm):
+    id = IntegerField("id", validators=[DataRequired()], default=-1)
+    name = StringField("Model:", validators=[DataRequired()])
+    status = SelectField(
+        "Status:", default='active',
+        choices=[('not_active', 'Not Active'), ('active', 'Active')]
+        )
