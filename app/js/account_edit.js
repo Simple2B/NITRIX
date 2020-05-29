4172c314fd6f3c27810c0dd35ca32fbe8fe78d81
MeitaArt <meitaartem@gmail.com>
2020-05-29 13:08:22 +0300

    js ext

--- app/js/account_edit.js
+++ app/js/account_edit.js
@@ -6,7 +6,20 @@ $(document).ready(function() {
         endDate.setMonth(endDate.getMonth() + month);
         $("#endDate")[0].valueAsDate = endDate;
     };
+
+    function update_end_date_ext() {
+        let endDate = new Date($('#extension_date').val());
+        let month = +$('#monthsExt').val();
+        endDate.setMonth(endDate.getMonth() + month);
+        $("#endDateExt")[0].valueAsDate = endDate;
+    };
+
     $('#activation_date').change(update_end_date);
     $('#months').change(update_end_date);
     update_end_date();
+
+    $('#extension_date').change(update_end_date_ext);
+    $('#monthsExt').change(update_end_date_ext);
+    update_end_date_ext();
 } );
+
