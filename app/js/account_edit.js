913953a078fd4fb246e122dfe4e05c177baadf09
MeitaArt <meitaartem@gmail.com>
2020-05-29 16:13:25 +0300

    js and account

--- app/js/account_edit.js
+++ app/js/account_edit.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
         $("#endDate")[0].valueAsDate = endDate;
     };
 
+    $('#activation_date').change(update_end_date);
+    $('#months').change(update_end_date);
+    update_end_date();
+
     function update_end_date_ext() {
         let endDate = new Date($('#extension_date').val());
         let month = +$('#monthsExt').val();
@@ -14,12 +18,33 @@ $(document).ready(function() {
         $("#endDateExt")[0].valueAsDate = endDate;
     };
 
-    $('#activation_date').change(update_end_date);
-    $('#months').change(update_end_date);
-    update_end_date();
-
     $('#extension_date').change(update_end_date_ext);
     $('#monthsExt').change(update_end_date_ext);
     update_end_date_ext();
+
+    document.querySelectorAll('.save_ext').forEach(row => {
+        function update_end_date_ext() {
+            let endDate = new Date(row.querySelector('.extension_datesave').value);
+            let month = +row.querySelector('.monthssave').value;
+            endDate.setMonth(endDate.getMonth() + month);
+            row.querySelector('.enddatesave').valueAsDate = endDate;
+        };
+        row.querySelector('.extension_datesave').addEventListener('change', update_end_date_ext);
+        row.querySelector('.monthssave').addEventListener('change', update_end_date_ext);
+        update_end_date_ext();
+    });
+
+
+
+    // function update_end_date_ext_save() {
+    //     let endDate = new Date($('.form-control extension_datesave').val());
+    //     let month = +$('.form-control monthssave').val();
+    //     endDate.setMonth(endDate.getMonth() + month);
+    //     $(".form-control enddatesave")[0].valueAsDate = endDate;
+    // };
+
+    // $('.form-control extension_datesave').change(update_end_date_ext_save);
+    // $('.form-control monthssave').change(update_end_date_ext_save);
+    // update_end_date_ext_save();
 } );
 
