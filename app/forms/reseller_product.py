cc38a53f4a2ea882c52fdd7bd5ceb3e1fb3fa913
Nikolay <nchernov@gmail.com>
2020-06-07 23:44:24 +0300

    next iteration

--- app/forms/reseller_product.py
+++ app/forms/reseller_product.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FloatField, SubmitField
+from wtforms import IntegerField, FloatField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class ResellerProductForm(FlaskForm):
     id = IntegerField("id", validators=[DataRequired()], default=-1)
     product_id = IntegerField("product_id:", validators=[DataRequired()])
-    product_name = IntegerField("Product:", validators=[DataRequired()])
+    product_name = StringField("Product:")
     reseller_id = IntegerField("Reseller:", validators=[DataRequired()])
     months = IntegerField("Months:", validators=[DataRequired()])
     init_price = FloatField("Init_Price:", validators=[DataRequired()])
--- app/views/reseller_product.py
+++ app/views/reseller_product.py
@@ -10,7 +10,7 @@ from app.logger import log
 reseller_product_blueprint = Blueprint('reseller_product', __name__)
 
 
-@reseller_product_blueprint.route("/add_reseller_product", methods=["GET"])
+@reseller_product_blueprint.route("/reseller_product_add")
 @login_required
 def add():
     reseller_id = int(request.args['reseller_id'])
@@ -20,15 +20,16 @@ def add():
         )
     form.is_edit = False
     form.save_route = url_for('reseller_product.save')
+    form.products = Product.query.filter(Product.deleted == False)  # noqa E712
     # form.delete_route = url_for('reseller_product.delete')
     form.products = Product.query.filter(Product.deleted == False)  # noqa E712
     return render_template(
-            "reseller_add_product.html",
+            "reseller_product.html",
             form=form
         )
 
 
-@reseller_product_blueprint.route("/edit_reseller_product", methods=["POST"])
+@reseller_product_blueprint.route("/reseller_product_edit", methods=["POST"])
 @login_required
 def edit():
     log(log.INFO, '/reseller_product_edit')
@@ -43,9 +44,20 @@ def edit():
         )
 
 
-@reseller_product_blueprint.route("/save_reseller_product", methods=["GET"])
+@reseller_product_blueprint.route("/reseller_product_save", methods=["POST"])
 @login_required
 def save():
-    log(log.INFO, '/reseller_product_edit')
+    log(log.INFO, '/reseller_product_save')
     form = ResellerProductForm(request.form)
-    return redirect(url_for('reseller.edit', id=form.reseller_id))
+    if not form.validate_on_submit():
+        flash('Form validation error', 'danger')
+        log(log.WARNING, "Form validation error %s", form.errors)
+        return redirect(url_for('reseller.edit', id=form.reseller_id.data))
+        # TODO: need check if same product already exists (with same product_id and months)
+    ResellerProduct(
+        reseller_id=form.product_id.data,
+        product_id=form.product_id.data,
+        months=form.months.data,
+        init_price=form.init_price.data,
+        ext_price=form.ext_price.data).save()
+    return redirect(url_for('reseller.edit', id=form.reseller_id.data))
