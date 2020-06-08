b8600ed7ef52c7c1901fd1f62a6dbfb7c461deee
Nikolay <nchernov@gmail.com>
2020-06-08 21:49:08 +0300

    reseller_product in separate form

--- app/views/reseller_product.py
+++ app/views/reseller_product.py
@@ -29,19 +29,51 @@ def add():
         )
 
 
-@reseller_product_blueprint.route("/reseller_product_edit", methods=["POST"])
+@reseller_product_blueprint.route("/reseller_product_remove", methods=['GET'])
+@login_required
+def delete():
+    log(log.INFO, '%s /reseller_product_remove', request.method)
+    if 'id' not in request.args:
+        flash('Unknown reseller product id', 'danger')
+        return redirect(url_for('main.resellers'))
+    product = ResellerProduct.query.filter(ResellerProduct.id == int(request.args['id'])).first()
+    reseller_id = product.reseller_id
+    product.delete()
+    return redirect(url_for('reseller.edit', id=reseller_id))
+
+
+@reseller_product_blueprint.route("/reseller_product_edit")
 @login_required
 def edit():
-    log(log.INFO, '/reseller_product_edit')
-    form = ResellerProductForm(request.form)
-    if not form.validate_on_submit():
-        flash('Form validation error', 'danger')
-        log(log.WARNING, "Form validation error")
-        return redirect(url_for('reseller.edit', id=form.reseller_id))
+    log(log.INFO, '%s /reseller_product_edit', request.method)
+    if request.method == 'GET':
+        if 'id' not in request.args:
+            flash('Unknown reseller product id', 'danger')
+            return redirect(url_for('main.resellers'))
+        product = ResellerProduct.query.filter(ResellerProduct.id == int(request.args['id'])).first()
+        if not product:
+            flash('Wrong reseller product id', 'danger')
+            return redirect(url_for('main.resellers'))
+        form = ResellerProductForm(
+            id=product.id,
+            product_id=product.product_id,
+            reseller_id=product.reseller_id,
+            months=product.months,
+            init_price=product.init_price,
+            ext_price=product.ext_price
+            )
+    else:
+        form = ResellerProductForm(request.form)
+        if not form.validate_on_submit():
+            flash('Form validation error', 'danger')
+            log(log.WARNING, "Form validation error")
+            return redirect(url_for('reseller.edit', id=form.reseller_id.data))
+    form.products = Product.query.filter(Product.deleted == False)  # noqa E712
+    form.save_route = url_for('reseller_product.save')
     return render_template(
-            "reseller_add_product.html",
-            form=form
-        )
+        "reseller_product.html",
+        form=form
+    )
 
 
 @reseller_product_blueprint.route("/reseller_product_save", methods=["POST"])
@@ -53,11 +85,19 @@ def save():
         flash('Form validation error', 'danger')
         log(log.WARNING, "Form validation error %s", form.errors)
         return redirect(url_for('reseller.edit', id=form.reseller_id.data))
-        # TODO: need check if same product already exists (with same product_id and months)
-    ResellerProduct(
-        reseller_id=form.product_id.data,
-        product_id=form.product_id.data,
-        months=form.months.data,
-        init_price=form.init_price.data,
-        ext_price=form.ext_price.data).save()
+    product = ResellerProduct.query\
+        .filter(ResellerProduct.reseller_id == form.reseller_id.data)\
+        .filter(ResellerProduct.product_id == form.product_id.data)\
+        .filter(ResellerProduct.months == form.months.data).first()
+    if product:
+        product.init_price = form.init_price.data
+        product.ext_price = form.ext_price.data
+    else:
+        product = ResellerProduct(
+            reseller_id=form.reseller_id.data,
+            product_id=form.product_id.data,
+            months=form.months.data,
+            init_price=form.init_price.data,
+            ext_price=form.ext_price.data)
+    product.save()
     return redirect(url_for('reseller.edit', id=form.reseller_id.data))
