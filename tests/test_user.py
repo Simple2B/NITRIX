9e5b0ee7c5c256960a73f486788ff22bf93eb001
Nikolay <nchernov@gmail.com>
2020-06-14 10:35:47 +0300

    test_user refactoring

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,12 @@
 import pytest
+from flask import url_for
 from app import db, create_app
 from app.models import User
-from .test_auth import register, login
+from .test_auth import register, login, logout
 
+
+TEST_USER_NAME = 'Test-User'
+TEST_USER_TYPE = User.Type.super_admin
 
 @pytest.fixture
 def client():
@@ -15,9 +19,9 @@ def client():
         db.drop_all()
         db.create_all()
         # register test user.
-        register('sam')
+        register('admin')
         # Login by test user
-        login(client, 'sam')
+        login(client, 'admin')
         yield client
         db.session.remove()
         db.drop_all()
@@ -25,12 +29,12 @@ def client():
 
 
 def test_edit_user(client):
-    response = client.get('/users')
+    response = client.get(url_for('main.users'))
     assert response.status_code == 200
     user = User(name='TEST USER NAME')
     user.password = "12345"
     user.save()
-    response = client.get(f'/user_edit?id={user.id}')
+    response = client.get(url_for('user.edit', id=user.id))
     assert response.status_code == 200
     assert b'TEST USER NAME' in response.data
 
@@ -38,7 +42,7 @@ def test_edit_user(client):
 def test_save_user(client):
     # add new USER
     response = client.post(
-        '/user_save',
+        url_for('user.save'),
         data=dict(id=-1, name='TEST USER NAME', password="12345"),
         follow_redirects=True
     )
@@ -47,7 +51,7 @@ def test_save_user(client):
 
     # edit exists user
     response = client.post(
-        '/user_save',
+        url_for('user.save'),
         data=dict(id=2, name='ANOTHER USER NAME', password="12345"),
         follow_redirects=True
     )
@@ -56,7 +60,7 @@ def test_save_user(client):
     assert b'ANOTHER USER NAME' in response.data
     # save user with wrong id
     response = client.post(
-        '/user_save',
+        url_for('user.save'),
         data=dict(id=3, name='BAD USER NAME', password="12345"),
         follow_redirects=True
     )
@@ -64,8 +68,26 @@ def test_save_user(client):
 
     # send wrong form data
     response = client.post(
-        '/user_save',
+        url_for('user.save'),
         data=dict(id=2, name='BAD USER NAME'),
         follow_redirects=True
     )
     assert b'Form validation error' in response.data
+
+
+def test_delete_user(client):
+    user_id = register(TEST_USER_NAME)
+    res = client.get(url_for('main.users'))
+    assert res.status_code == 200
+    assert f'{TEST_USER_NAME}'.encode() in res.data
+    res = client.get(url_for('user.delete', id=user_id))
+    assert res.status_code == 302  # redirected to main.users
+    assert f'{TEST_USER_NAME}'.encode() not in res.data
+    # try delete user w/o authorization
+    user_id = register(TEST_USER_NAME)
+    logout(client)
+    res = client.get(url_for('user.delete', id=user_id))
+    assert res.status_code == 302  # redirected to main.users
+    login(client, 'admin')
+    res = client.get(url_for('main.users'))
+    assert f'{TEST_USER_NAME}'.encode() in res.data
