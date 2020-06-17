0532f4bc96cdc1185ec5cc3d0c55c1bf75c43f39
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-17 16:56:11 +0300

    Updated testing

--- tests/test_account_extension.py
+++ tests/test_account_extension.py
@@ -1,25 +1,18 @@
 import pytest
 from flask import url_for
 from app import create_app, db
-from app.models import AccountExtension, Account
+from app.models import AccountExtension, Account, Product, Phone, Reseller
 from datetime import datetime
-from .test_auth import register, login
+from .test_auth import register, login, logout
 
-OK = 200
-REDIRECTED = 302
-NOT_NUMBER_ID = 'text_id'
 CORRECT_ID = '1'
-ENCODING = 'utf-8'
+NOT_NUMBER_ID = 'text_id'
+CORRECT_MONTHS = 12
+WRONG_MONTHS = 13
 LOGIN = 'sam'
-PASSW = '1234'
-EXTENSION_ADD = 'account_extension.add'
-EXTENSION_EDIT = 'account_extension.edit'
-EXTENSION_SAVE_NEW = 'account_extension.save_new'
-EXTENSION_SAVE_UPDATE = 'account_extension.save_update'
-EXTENSION_DELETE = 'account_extension.delete'
 UNKNOWN_ID = 'Unknown id'
+MONTHS_ERROR = 'Months must be in 1-12'
 VALIDATION_ERROR = 'Form validation error'
-WRONG_MONTHS = 'Months must be in 1-12'
 
 
 @pytest.fixture
@@ -30,7 +23,7 @@ def client():
         app_ctx = app.app_context()
         app_ctx.push()
         db.create_all()
-        register(LOGIN, PASSW)  # register a test user.
+        register(LOGIN)  # register a test user.
         yield client
         db.session.remove()
         db.drop_all()
@@ -38,163 +31,127 @@ def client():
 
 
 def test_add(client):
-    # what if not authorised
-    response = client.get(url_for(EXTENSION_ADD))
-    assert response.status_code == REDIRECTED  # to the login page
-
-    login(client, LOGIN, PASSW)
-    # what if id is empty
-    response = client.get(url_for(EXTENSION_ADD, id=''))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not into a request
-    response = client.get(url_for(EXTENSION_ADD))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not a number
-    response = client.get(url_for(EXTENSION_ADD), id=NOT_NUMBER_ID)
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
+    check_not_auth(client, 'account_extension.add')
+    login(client, LOGIN)
+    check_id(client, 'account_extension.add')
     # what if correct id
-    response = client.get(url_for(EXTENSION_ADD, id=CORRECT_ID))
-    assert response.status_code == OK
-
-
-def test_edit():
-    # what if not authorised
-    response = client.get(url_for(EXTENSION_EDIT))
-    assert response.status_code == REDIRECTED  # to the login page
-
-    login(client, LOGIN, PASSW)
-    # what if id is empty
-    response = client.get(url_for(EXTENSION_EDIT, id=''))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not into a request
-    response = client.get(url_for(EXTENSION_EDIT))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not a number
-    response = client.get(url_for(EXTENSION_EDIT), id=NOT_NUMBER_ID)
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is correct, but an account_extension is not found
-    response = client.get(url_for(EXTENSION_EDIT, id=CORRECT_ID))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is correct, but an account_extension exists
-    extension = AccountExtension(id=1)
-    extension.save()
-    response = client.get(url_for(EXTENSION_EDIT, id=CORRECT_ID))
+    response = client.get(url_for('account_extension.add', id=CORRECT_ID))
     assert response.status_code == 200
-    assert response.form.get('id') == '1'
 
 
-def test_save_new():
-    # what if not authorised
-    response = client.get(url_for(EXTENSION_SAVE_NEW))
-    assert response.status_code == REDIRECTED  # to the login page
+def test_edit(client):
+    check_not_auth('account_extension.edit')
+    login(client, LOGIN)
+    check_id('account_extension.edit')
+    # what if id is correct, but an account_extension is not found
+    response = client.get(url_for('account_extension.edit', id=CORRECT_ID))
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if id is correct, but an account_extension exists
+    extension = AccountExtension()
+    extension.save()
+    ext_id = extension.id
+    response = client.get(url_for('account_extension.edit', id=CORRECT_ID))
+    assert response.status_code == 200
+    assert response.form.get('id') == str(ext_id)
 
-    login(client, LOGIN, PASSW)
-    # what if account_id is empty
-    response = client.post(url_for(EXTENSION_EDIT), data=dict(id=''))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if account_id isn't a number
-    response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=NOT_NUMBER_ID)
-    )
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
+
+def test_save_new(client):
+    check_not_auth('account_extension.save_new')
+    login(client, LOGIN)
+    check_id_post(client, 'account_extension.save_new')
     # what if no account by an account_id
     response = client.post(
-        url_for(EXTENSION_EDIT),
+        url_for('account_extension.save_new'),
         data=dict(
             id=CORRECT_ID,
-            reseller_id='1',
+            reseller_id=CORRECT_ID,
             extension_date=datetime.now,
-            product_id='1', months=1
+            product_id=CORRECT_ID, months=CORRECT_MONTHS
         )
     )
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-
-    account = Account(
-        id=CORRECT_ID,
-        name='TEST_ACCOUNT',
-        product_id=1,
-        phone_id=1,
-        reseller_id=1,
-        sim='TEST_SIM',
-        imei='TEST_IMEI',
-        comment='TEST_COMMENT'
-    )
-    account.save()
-    # what if pass on reseller_id
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if pass on reseller id
+    ids = set_data()
     response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=UNKNOWN_ID, extension_date=datetime.now, product_id='1', months=1)
+        url_for('account_extension.save_new'),
+        data=dict(
+            id=ids['account_id'],
+            extension_date=datetime.now,
+            product_id=ids['product_id'],
+            months=CORRECT_MONTHS
+        )
     )
-    assert response.status_code == REDIRECTED  # to the account.edit
-    assert bytes(VALIDATION_ERROR, encoding=ENCODING) in response.data
+    assert response.status_code == 302  # to the account.edit
+    assert VALIDATION_ERROR.encode() in response.data
     # what if pass on extension_date
     response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=CORRECT_ID, reseller_id='1', product_id='1', months=1)
+        url_for('account_extension.save_new'),
+        data=dict(
+            id=ids['account_id'],
+            reseller_id=ids['reseller_id'],
+            product_id=ids['product_id'],
+            months=CORRECT_MONTHS
+        )
     )
-    assert response.status_code == REDIRECTED  # to the account.edit
-    assert bytes(VALIDATION_ERROR, encoding=ENCODING) in response.data
+    assert response.status_code == 302  # to the account.edit
+    assert VALIDATION_ERROR.encode() in response.data
     # what if pass on product_id
     response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=CORRECT_ID, reseller_id='1', extension_date=datetime.now, months=1)
+        url_for('account_extension.save_new'),
+        data=dict(
+            id=ids['account_id'],
+            reseller_id=ids['reseller_id'],
+            extension_date=datetime.now,
+            months=CORRECT_MONTHS
+        )
     )
-    assert response.status_code == REDIRECTED  # to the account.edit
-    assert bytes(VALIDATION_ERROR, encoding=ENCODING) in response.data
+    assert response.status_code == 302  # to the account.edit
+    assert VALIDATION_ERROR.encode() in response.data
     # what if pass on months
     response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=CORRECT_ID, reseller_id='1', extension_date=datetime.now, product_id='1')
-    )
-    assert response.status_code == REDIRECTED  # to the account.edit
-    assert bytes(VALIDATION_ERROR, encoding=ENCODING) in response.data
-    # Wrong months
-    response = client.post(
-        url_for(EXTENSION_EDIT),
-        data=dict(id=CORRECT_ID, reseller_id='1', extension_date=datetime.now, product_id='1', months=13)
-    )
-    assert response.status_code == REDIRECTED  # to the account.edit
-    assert bytes(WRONG_MONTHS, encoding=ENCODING) in response.data
-    # what if OK
-    current_dt = datetime.now
-    response = client.post(
-        url_for(EXTENSION_EDIT),
+        url_for('account_extension.save_new'),
         data=dict(
-            id=CORRECT_ID, reseller_id='2',
-            extension_date=current_dt, product_id='2', months=1)
+            id=ids['account_id'],
+            reseller_id=ids['reseller_id'],
+            extension_date=datetime.now,
+            product_id=ids['product_id'])
     )
-    extensions = AccountExtension.query.filter(AccountExtension.account_id == CORRECT_ID).all()
-    assert response.status_code == OK
+    assert response.status_code == 302  # to the account.edit
+    assert VALIDATION_ERROR.encode() in response.data
+    # what if wrong months
+    response = client.post(
+        url_for('account_extension.save_new'),
+        data=dict(
+            id=ids['account_id'],
+            reseller_id=ids['reseller_id'],
+            extension_date=datetime.now,
+            product_id=ids['product_id'],
+            months=WRONG_MONTHS)
+    )
+    assert response.status_code == 302  # to the account.edit
+    assert MONTHS_ERROR.encode in response.data
+    # what if OK
+    response = client.post(
+        url_for('account_extension.save_new'),
+        data=dict(
+            id=ids['account_id'],
+            reseller_id=ids['reseller_id'],
+            extension_date=datetime.now,
+            product_id=ids['product_id'],
+            months=CORRECT_MONTHS
+        )
+    )
+    extensions = AccountExtension.query.filter(AccountExtension.account_id == ids['account_id']).all()
+    assert response.status_code == 200
     assert extensions
 
 
-def test_save_update():
-    # what if not authorised
-    response = client.get(url_for(EXTENSION_SAVE_UPDATE))
-    assert response.status_code == REDIRECTED  # to the login page
-
-    login(client, LOGIN, PASSW)
-    # what if id is empty
-    response = client.post(
-        url_for(EXTENSION_SAVE_UPDATE),
-        data=dict(id=''))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not into request
-    response = client.post(url_for(EXTENSION_SAVE_UPDATE))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
+def test_save_update(client):
+    check_not_auth(client, 'account_extension.save_update')
+    login(client, LOGIN)
+    check_id_post(client, 'account_extension.save_update')
     # what if pass not all data
 
     # what if wrong data
@@ -204,30 +161,83 @@ def test_save_update():
 
 
 def test_delete():
-    # what if not authorised
-    response = client.get(url_for(EXTENSION_DELETE))
-    assert response.status_code == REDIRECTED  # to the login page
-
-    login(client, LOGIN, PASSW)
-    # what if id is empty
-    response = client.get(url_for(EXTENSION_DELETE, id=''))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not into request
-    response = client.get(url_for(EXTENSION_DELETE))
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
-    # what if id is not a number
-    response = client.get(url_for(EXTENSION_DELETE), id=NOT_NUMBER_ID)
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
+    check_not_auth(client, 'account_extension.delete')
+    login(client, LOGIN)
+    check_id(client, 'account_extension.delete')
     # what if delete an extension which is not existed
-    response = client.get(url_for(EXTENSION_DELETE), id=CORRECT_ID)
-    assert response.status_code == REDIRECTED  # to the main.accounts
-    assert bytes(UNKNOWN_ID, encoding=ENCODING) in response.data
+    ads = set_data()
+    response = client.get(url_for('account_extension.delete'), id=CORRECT_ID)
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
     # what if delete existed extension
-    extension = AccountExtension(id=CORRECT_ID, account_id=10)
-    extension.save()
-    response = client.get(url_for(EXTENSION_DELETE), id=CORRECT_ID)
-    assert response.status_code == REDIRECTED
-    assert response.data.get('id') == '10'
+    ads = set_data()
+    acc_ext_id = AccountExtension(**ads).save().id
+    response = client.get(url_for('account_extension.delete'), id=acc_ext_id)
+    assert response.status_code == 302
+    assert response.data.get('id') == ads['account_id']
+
+
+def check_id(client, blueprint):
+    # what if id is empty
+    response = client.get(url_for(blueprint, id=''))
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if id is not into a request
+    response = client.get(url_for(blueprint))
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if id is not a number
+    response = client.get(url_for(blueprint), id=NOT_NUMBER_ID)
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+
+
+def check_id_post(client, blueprint):
+    # what if account_id is empty
+    response = client.post(
+        url_for(blueprint),
+        data=dict(id='')
+    )
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if id is not into request
+    response = client.post(
+        url_for(blueprint)
+    )
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+    # what if account_id isn't a number
+    response = client.post(
+        url_for(blueprint),
+        data=dict(id=NOT_NUMBER_ID)
+    )
+    assert response.status_code == 302  # to the main.accounts
+    assert UNKNOWN_ID.encode() in response.data
+
+
+def check_not_auth(client, blueprint):
+    # what if not authorised
+    logout(client)
+    response = client.get(url_for(blueprint))
+    assert response.status_code == 302  # to the login page
+
+
+def set_data():
+    prod_id = Product(name="TEST_NAME", status=Product.Status.active).save().id
+    phone_id = Phone(name="TEST_NAME", status=Phone.Status.active).save().id
+    res_id = Reseller(name="TEST_NAME", status=Reseller.Status.active).save().id
+    acc_id = Account(
+        name='TEST_NAME',
+        product_id=prod_id,
+        phone_id=phone_id,
+        reseller_id=res_id,
+        sim='TEST_SIM',
+        imei='TEST_IMEI',
+        comment='TEST_COMMENT'
+    ).save().id
+    return {
+        'product_id': prod_id,
+        'phone_id': phone_id,
+        'reseller_id': res_id,
+        'account_id': acc_id
+    }
