8c6fcee6b5df6ecbaa986366e6bcf4242a66344e
Nikolay Chernov <nchernov@gmail.com>
2020-07-01 17:15:33 +0300

    edit acc dont write to ninja

--- app/views/account.py
+++ app/views/account.py
@@ -2,7 +2,14 @@ from datetime import datetime
 from flask import render_template, Blueprint, request, flash, redirect, url_for
 from flask_login import login_required
 
-from app.models import Account, Product, Reseller, AccountExtension, AccountChanges, Phone
+from app.models import (
+    Account,
+    Product,
+    Reseller,
+    AccountExtension,
+    AccountChanges,
+    Phone,
+)
 from app.models import ResellerProduct
 from app.forms import AccountForm
 from ..database import db
@@ -12,7 +19,7 @@ from app.utils import ninja_product_name
 from app.ninja import api as ninja
 
 
-account_blueprint = Blueprint('account', __name__)
+account_blueprint = Blueprint("account", __name__)
 
 
 def all_phones():
@@ -24,14 +31,14 @@ def all_phones():
 @account_blueprint.route("/account_details")
 @login_required
 def edit():
-    log(log.INFO, '/account_details')
-    if 'id' in request.args:
-        id = int(request.args['id'])
+    log(log.INFO, "/account_details")
+    if "id" in request.args:
+        id = int(request.args["id"])
         account = Account.query.filter(Account.id == id).first()
         if account is None:
             flash("Wrong account id.", "danger")
-            log(log.ERROR, 'Wrong account id.')
-            return redirect(url_for('main.accounts'))
+            log(log.ERROR, "Wrong account id.")
+            return redirect(url_for("main.accounts"))
         form = AccountForm(
             id=account.id,
             name=account.name,
@@ -42,82 +49,88 @@ def edit():
             imei=account.imei,
             comment=account.comment,
             activation_date=account.activation_date,
-            months=account.months
-            )
+            months=account.months,
+        )
         form.products = Product.query.filter(Product.deleted == False)  # noqa E712
         form.resellers = Reseller.query.filter(Reseller.deleted == False)  # noqa E712
         form.phones = all_phones()
-        form.extensions = AccountExtension.query.filter(AccountExtension.account_id == form.id.data).all()
-        form.name_changes = AccountChanges.query.filter(AccountChanges.account_id == form.id.data).filter(
-                AccountChanges.change_type == AccountChanges.ChangeType.name).all()
-        form.sim_changes = AccountChanges.query.filter(
-            AccountChanges.account_id == form.id.data).filter(
-                AccountChanges.change_type == AccountChanges.ChangeType.sim).all()
+        form.extensions = AccountExtension.query.filter(
+            AccountExtension.account_id == form.id.data
+        ).all()
+        form.name_changes = (
+            AccountChanges.query.filter(AccountChanges.account_id == form.id.data)
+            .filter(AccountChanges.change_type == AccountChanges.ChangeType.name)
+            .all()
+        )
+        form.sim_changes = (
+            AccountChanges.query.filter(AccountChanges.account_id == form.id.data)
+            .filter(AccountChanges.change_type == AccountChanges.ChangeType.sim)
+            .all()
+        )
         form.is_edit = True
-        form.save_route = url_for('account.save')
-        form.delete_route = url_for('account.delete')
-        form.close_button = url_for('main.accounts')
+        form.save_route = url_for("account.save")
+        form.delete_route = url_for("account.delete")
+        form.close_button = url_for("main.accounts")
         form.reseller_name = account.reseller.name
-        return render_template(
-                "account_details.html",
-                form=form
-            )
+        return render_template("account_details.html", form=form)
     else:
         form = AccountForm()
         form.products = Product.query.all()
         form.resellers = Reseller.query.all()
-        form.phones = Phone.query.filter(Phone.deleted == False).filter(Phone.status == Phone.Status.active).all()  # noqa E712
+        form.phones = (
+            Phone.query.filter(Phone.deleted == False)  # noqa E712
+            .filter(Phone.status == Phone.Status.active)
+            .all()
+        )  # noqa E712
         form.is_edit = False
-        form.save_route = url_for('account.save')
-        form.delete_route = url_for('account.delete')
-        form.close_button = url_for('main.accounts')
-        return render_template(
-                "account_details.html",
-                form=form
-            )
+        form.save_route = url_for("account.save")
+        form.delete_route = url_for("account.delete")
+        form.close_button = url_for("main.accounts")
+        return render_template("account_details.html", form=form)
 
 
-def add_ninja_invoice(account: Account): # noqa E999
-    reseller_product = ResellerProduct.query.filter(
-        ResellerProduct.reseller_id == account.reseller_id
-        ).filter(
-            ResellerProduct.product_id == account.product_id
-        ).filter(
-            ResellerProduct.months == account.months
-        ).first()
+def add_ninja_invoice(account: Account):
+    reseller_product = (
+        ResellerProduct.query.filter(ResellerProduct.reseller_id == account.reseller_id)
+        .filter(ResellerProduct.product_id == account.product_id)
+        .filter(ResellerProduct.months == account.months)
+        .first()
+    )
     if not reseller_product:
         # Locking for this product in NITRIX reseller
-        reseller_product = ResellerProduct.query.filter(
-            ResellerProduct.reseller_id == 1
-        ).filter(
-            ResellerProduct.product_id == account.product_id
-        ).filter(
-            ResellerProduct.months == account.months
-        ).first()
+        reseller_product = (
+            ResellerProduct.query.filter(ResellerProduct.reseller_id == 1)
+            .filter(ResellerProduct.product_id == account.product_id)
+            .filter(ResellerProduct.months == account.months)
+            .first()
+        )
     # First day of month
     invoice_date = datetime(datetime.now().year, datetime.now().month, 1)
-    invoice_date = invoice_date.strftime('%Y-%m-%d')
+    invoice_date = invoice_date.strftime("%Y-%m-%d")
     current_invoice = None
     for invoice in NinjaInvoice.all():
-        if invoice.invoice_date == invoice_date and invoice.client_id == account.reseller.ninja_client_id:
+        if (
+            invoice.invoice_date == invoice_date
+            and invoice.client_id == account.reseller.ninja_client_id
+        ):
             # found invoice
             current_invoice = invoice
             break
     else:
         # need a new invoice
-        current_invoice = NinjaInvoice.add(account.reseller.ninja_client_id, invoice_date)
+        current_invoice = NinjaInvoice.add(
+            account.reseller.ninja_client_id, invoice_date
+        )
     if current_invoice:
         current_invoice.add_item(
             ninja_product_name(account.product.name, account.months),
             account.name,
-            cost=reseller_product.init_price if reseller_product else 0)
+            cost=reseller_product.init_price if reseller_product else 0,
+        )
     if account.phone.name != "None":
         phone_name = f"Phone-{account.phone.name}"
         if current_invoice:
-            current_invoice.add_item(
-                phone_name,
-                account.name,
-                cost=account.phone.price)
+            current_invoice.add_item(phone_name, account.name, cost=account.phone.price)
 
 
 @account_blueprint.route("/account_save", methods=["POST"])
@@ -128,6 +141,7 @@ def save():
     if form.validate_on_submit():
         form.name.data = form.name.data.strip()
         form.sim.data = form.sim.data.strip()
+        new_account = False
         if form.id.data > 0:
             # Edit exists account
             account = Account.query.filter(Account.id == form.id.data).first()
@@ -148,20 +162,24 @@ def save():
                 account.__setattr__(k, form.__getattribute__(k).data)
         else:
             # Add a new account
-            account = Account(name=form.name.data, product_id=form.product_id.data,
-                              reseller_id=form.reseller_id.data,
-                              phone_id=form.phone_id.data,
-                              sim=form.sim.data,
-                              imei=form.imei.data,
-                              comment=form.comment.data,
-                              activation_date=form.activation_date.data,
-                              months=form.months.data)
+            new_account = True
+            account = Account(
+                name=form.name.data,
+                product_id=form.product_id.data,
+                reseller_id=form.reseller_id.data,
+                phone_id=form.phone_id.data,
+                sim=form.sim.data,
+                imei=form.imei.data,
+                comment=form.comment.data,
+                activation_date=form.activation_date.data,
+                months=form.months.data,
+            )
         # Check that months must be in 1-12
         if not 0 < account.months <= 12:
-            flash('Months must be in 1-12', 'danger')
-            return redirect(url_for('account.edit', id=account.id))
+            flash("Months must be in 1-12", "danger")
+            return redirect(url_for("account.edit", id=account.id))
         account.save()
-        if ninja.configured:
+        if new_account and ninja.configured:
             add_ninja_invoice(account)
         # Change Resellers last activity
         reseller = Reseller.query.filter(Reseller.id == account.reseller_id).first()
@@ -169,37 +187,39 @@ def save():
         reseller.save()
 
         log(log.INFO, "Account data was saved")
-        if request.form['submit'] == 'save_and_add':
-            return redirect(url_for('account.edit'))
-        if request.form['submit'] == 'save_and_edit':
-            return redirect(url_for('account.edit', id=account.id))
-        return redirect(url_for('main.accounts'))
+        if request.form["submit"] == "save_and_add":
+            return redirect(url_for("account.edit"))
+        if request.form["submit"] == "save_and_edit":
+            return redirect(url_for("account.edit", id=account.id))
+        return redirect(url_for("main.accounts"))
     else:
-        flash('Form validation error', 'danger')
+        flash("Form validation error", "danger")
         log(log.ERROR, "Form validation error")
-    return redirect(url_for('account.edit', id=form.id.data))
+    return redirect(url_for("account.edit", id=form.id.data))
 
 
 @account_blueprint.route("/account_delete", methods=["GET"])
 @login_required
 def delete():
-    if 'id' in request.args:
-        account_id = int(request.args['id'])
+    if "id" in request.args:
+        account_id = int(request.args["id"])
         Account.query.filter(Account.id == account_id).delete()
         db.session.commit()
-        return redirect(url_for('main.accounts'))
-    flash('Wrong request', 'danger')
-    return redirect(url_for('main.accounts'))
+        return redirect(url_for("main.accounts"))
+    flash("Wrong request", "danger")
+    return redirect(url_for("main.accounts"))
 
 
 @account_blueprint.route("/account_change_delete", methods=["GET"])
 @login_required
 def delete_change():
-    log(log.INFO, '%s /account_change_delete', request.method)
-    if 'id' not in request.args:
-        flash('Unknown Change id', 'danger')
-        return redirect(url_for('main.accounts'))
-    account_change = AccountChanges.query.filter(AccountChanges.id == int(request.args['id'])).first()
+    log(log.INFO, "%s /account_change_delete", request.method)
+    if "id" not in request.args:
+        flash("Unknown Change id", "danger")
+        return redirect(url_for("main.accounts"))
+    account_change = AccountChanges.query.filter(
+        AccountChanges.id == int(request.args["id"])
+    ).first()
     account_id = account_change.account_id
     account_change.delete()
-    return redirect(url_for('account.edit', id=account_id))
+    return redirect(url_for("account.edit", id=account_id))
--- tests/test_reseller_product.py
+++ tests/test_reseller_product.py
@@ -12,8 +12,8 @@ NEW_EXT_PRICE = 4444.44
 
 @pytest.fixture
 def client():
-    app = create_app(environment='testing')
-    app.config['TESTING'] = True
+    app = create_app(environment="testing")
+    app.config["TESTING"] = True
 
     with app.test_client() as client:
         app_ctx = app.app_context()
@@ -21,9 +21,9 @@ def client():
         db.drop_all()
         db.create_all()
         # register test user.
-        register('sam')
+        register("sam")
         # Login by test user
-        login(client, 'sam')
+        login(client, "sam")
         yield client
         db.session.remove()
         db.drop_all()
@@ -34,74 +34,108 @@ def test_edit_reseller(client):
     TEST_PROD_ID = 3
     reseller_product = ResellerProduct(product_id=TEST_PROD_ID)
     reseller_product.save()
-    url = url_for('reseller_product.edit', id=reseller_product.id)
+    url = url_for("reseller_product.edit", id=reseller_product.id)
     response = client.get(url)
     assert response.status_code == 200
-    assert f'{TEST_PROD_ID}'.encode('utf-8') in response.data
+    assert f"{TEST_PROD_ID}".encode("utf-8") in response.data
 
 
 def test_save_delete_reseller(client):
     # add new reseller
-    reseller = Reseller(name='Dima')
+    reseller = Reseller(name="Dima")
     reseller.save()
     product = Product(name="Gold")
     product.save()
     response = client.post(
-        url_for('reseller_product.save'),
+        url_for("reseller_product.save"),
         data=dict(
-            id=-1, product_id=product.id, reseller_id=reseller.id, months=3, init_price=INIT_PRICE, ext_price=EXT_PRICE),
-        follow_redirects=True
+            id=-1,
+            product_id=product.id,
+            reseller_id=reseller.id,
+            months=3,
+            init_price=INIT_PRICE,
+            ext_price=EXT_PRICE,
+        ),
+        follow_redirects=True,
     )
     assert response.status_code == 200
-    assert b'55555' in response.data
+    assert b"55555" in response.data
     # edit exists resellers product
     response = client.post(
-         url_for('reseller_product.save'),
-         data=dict(
-             id=1, product_id=product.id, reseller_id=reseller.id, months=3, init_price=NEW_INIT_PRICE, ext_price=NEW_EXT_PRICE),
-         follow_redirects=True
-     )
+        url_for("reseller_product.save"),
+        data=dict(
+            id=1,
+            product_id=product.id,
+            reseller_id=reseller.id,
+            months=3,
+            init_price=NEW_INIT_PRICE,
+            ext_price=NEW_EXT_PRICE,
+        ),
+        follow_redirects=True,
+    )
     assert response.status_code == 200
-    assert b'55555' not in response.data
-    assert b'88888' in response.data
+    assert b"55555" not in response.data
+    assert b"88888" in response.data
     # send wrong form data
     response = client.post(
-         url_for('reseller_product.save'),
-         data=dict(
-             id=1, product_id=product.id, reseller_id=reseller.id, months="Mohths", init_price=888.00, ext_price="25"),
-         follow_redirects=True
-     )
-    assert b'Form validation error' in response.data
+        url_for("reseller_product.save"),
+        data=dict(
+            id=1,
+            product_id=product.id,
+            reseller_id=reseller.id,
+            months="Mohths",
+            init_price=888.00,
+            ext_price="25",
+        ),
+        follow_redirects=True,
+    )
+    assert b"Form validation error" in response.data
     # delete
-    reseller_product = ResellerProduct(product_id=product.id, reseller_id=reseller.id, months=6,
-                                            init_price=INIT_PRICE, ext_price=EXT_PRICE)
+    reseller_product = ResellerProduct(
+        product_id=product.id,
+        reseller_id=reseller.id,
+        months=6,
+        init_price=INIT_PRICE,
+        ext_price=EXT_PRICE,
+    )
     reseller_product.save()
-    response = client.get(url_for('reseller_product.delete', id=reseller_product.id))
+    response = client.get(url_for("reseller_product.delete", id=reseller_product.id))
     assert response.status_code == 302
     assert b"55555" not in response.data
 
 
 def test_save_exist_product(client):
     # add new product
-    reseller = Reseller(name='Dima')
+    reseller = Reseller(name="Dima")
     reseller.save()
     product = Product(name="Gold")
     product.save()
     response = client.post(
-        url_for('reseller_product.save'),
+        url_for("reseller_product.save"),
         data=dict(
-            id=-1, product_id=product.id, reseller_id=reseller.id, months=3, init_price=INIT_PRICE, ext_price=EXT_PRICE),
-        follow_redirects=True
+            id=-1,
+            product_id=product.id,
+            reseller_id=reseller.id,
+            months=3,
+            init_price=INIT_PRICE,
+            ext_price=EXT_PRICE,
+        ),
+        follow_redirects=True,
     )
     assert response.status_code == 200
-    assert b'55555' in response.data
+    assert b"55555" in response.data
     # add the same product
     response = client.post(
-        url_for('reseller_product.save'),
+        url_for("reseller_product.save"),
         data=dict(
-            id=-1, product_id=product.id, reseller_id=reseller.id, months=3, init_price=INIT_PRICE,
-            ext_price=EXT_PRICE),
-        follow_redirects=True
+            id=-1,
+            product_id=product.id,
+            reseller_id=reseller.id,
+            months=3,
+            init_price=INIT_PRICE,
+            ext_price=EXT_PRICE,
+        ),
+        follow_redirects=True,
     )
     assert response.status_code == 200
-    assert b'Reseller already have this product' in response.data
+    assert b"Reseller already have this product" in response.data
