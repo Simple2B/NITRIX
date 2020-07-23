1d0a02d0a988812d5499d63ae5d185ea3a6cfb1b
macchester92 <radchenkovictor@yahoo.com>
2020-07-23 14:02:00 +0400

    added views to set up totp

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, url_for, redirect, flash, request
+from flask import Blueprint, render_template, url_for, redirect, flash, request, session, abort
 from flask_login import login_user, logout_user, login_required
+from io import BytesIO
 
 from app.models import User
 from app.forms import LoginForm
 from app.forms import ChangePasswordForm
 from app.logger import log
+
+import pyqrcode
 
 auth_blueprint = Blueprint("auth", __name__)
 
@@ -17,7 +20,9 @@ def login():
         # check if user has active two-factor auth
         user = User.query.filter(form.user_name.data).first()
         if not user.otp_active:
-            return 'Set up Two Factor Authentication first.'
+            # pass corresponding user_name with redirect
+            session['user_name'] = user.user_name
+            return redirect(url_for('auth.two_factor_setup'))
         user = User.authenticate(form.user_name.data, form.password.data)
         if user is not None:
             if user.verify_totp(form.token.data):
@@ -34,7 +39,39 @@ def login():
 
 @auth_blueprint.route('/two_factor_setup')
 def two_factor_setup():
-    pass
+    if 'username' not in session:
+        return redirect(url_for('auth.login'))
+    user = User.query.filter_by(user_name=session['user_name']).first()
+    if user is None:
+        return redirect(url_for('auth.login'))
+    # render QR code without caching it in browser
+    return render_template('two-factor-setup.html'), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+
+@auth_blueprint.route('/qrcode')
+def qrcode():
+    if 'user_name' not in session:
+        abort(404)
+    user = User.query.filter_by(user_name=session['user_name']).first()
+    if user is None:
+        abort(404)
+
+    # remove user_name from session for added security
+    del session['user_name']
+
+    # render qrcode for Google Authenticator
+    url = pyqrcode.create(user.get_totp_uri())
+    stream = BytesIO()
+    url.svg(stream, scale=3)
+    return stream.getvalue(), 200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
 
 @auth_blueprint.route("/logout")
 @login_required
