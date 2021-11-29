ab7cc160b779364f53ae33ad09d96943886dcd70
Andy-Ray <13idol13@gmail.com>
2021-11-29 16:51:55 +0200

    added: db dumper and remove old data

--- app/worker.py
+++ app/worker.py
@@ -1,7 +1,8 @@
 import subprocess
 from celery import Celery
-
+from celery.schedules import crontab
 from config import BaseConfig as conf
+from app.dumper import db_dumper
 
 
 celery = Celery(__name__)
@@ -15,9 +16,15 @@ def setup_periodic_tasks(sender, **kwargs):
         ninja_sync.s(),
         name="sync",
     )
+    sender.add_periodic_task(crontab(minute=56, hour=23), pg_dumb.s(), name="pg_dumb")
 
 
 @celery.task
 def ninja_sync():
     flask_proc = subprocess.Popen(["flask", "scheduler-task"])
     flask_proc.communicate()
+
+
+@celery.task
+def pg_dumb():
+    db_dumper()
